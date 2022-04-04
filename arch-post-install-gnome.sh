1ab2f3111acf5258d4a0fe8584b208493bcc1edb
juansquintero <32658886+juansquintero@users.noreply.github.com>
2022-04-04 07:28:52 -0500

    remove xf86 syanptics

--- arch-post-install-gnome.sh
+++ arch-post-install-gnome.sh
@@ -13,7 +13,7 @@ date
 hwclock -w
 
 # Xorg install
-pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu xf86-input-synaptics amd-ucode android-udev pipewire-pulse wireplumber ttf-droid noto-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts alacarte
+pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu libinput amd-ucode android-udev pipewire-pulse wireplumber ttf-droid noto-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts alacarte
 
 # Gnome install
 pacman -S --noconfirm gnome-shell nautilus gnome-terminal guake gnome-tweak-tool gnome-control-center xdg-user-dirs-gtk gdm gnome-keyring mutter cheese file-roller gedit gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-menus gnome-screenshot gnome-session gnome-settings-daemon gnome-system-monitor gnome-themes-extra
