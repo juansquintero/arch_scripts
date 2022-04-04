8c71e10baeeb3c83fcd5470aefbd8a425f34aa9c
juansquintero <32658886+juansquintero@users.noreply.github.com>
2022-04-03 23:54:37 -0500

    Update arch-post-install-gnome.sh

--- arch-post-install-gnome.sh
+++ arch-post-install-gnome.sh
@@ -13,7 +13,7 @@ date
 hwclock -w
 
 # Xorg install
-pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu xf86-input-synaptics amd-ucode android-udev
+pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu xf86-input-synaptics amd-ucode android-udev pipewire-pulse wireplumber
 
 # Gnome install
 pacman -S --noconfirm gnome-shell nautilus gnome-terminal guake gnome-tweak-tool gnome-control-center xdg-user-dirs-gtk gdm gnome-keyring mutter cheese file-roller gedit gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-menus gnome-screenshot gnome-session gnome-settings-daemon gnome-system-monitor
@@ -23,8 +23,8 @@ pacman -S --noconfirm chromium btop
 
 # yay install 
 pacman -S --needed --noconfirm git base-devel
-chmod 777 /yay_install.bash
-sudo -u juansquintero /yay_install.bash
+chmod 777 /yay-install.bash
+sudo -u juansquintero /yay-install.bash
 
 # Enable gnome lockscreen
 systemctl enable gdm 
