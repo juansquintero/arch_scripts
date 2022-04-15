4a9b019e9cac4ac6fa75cf7a977cf091fe803086
juansquintero <32658886+juansquintero@users.noreply.github.com>
2022-04-15 02:31:21 -0500

    add alacritty install

--- arch-post-install-gnome.sh
+++ arch-post-install-gnome.sh
@@ -3,7 +3,7 @@
 useradd -m -g users -G audio,video,network,wheel,storage,rfkill -s /bin/zsh juansquintero
 passwd juansquintero
 sed -i "s/#\ %wheel\ ALL=(ALL:ALL)\ ALL/%wheel\ ALL=(ALL:ALL)\ ALL/ " /etc/sudoers
-pacman -S --noconfirm acpid ntp dbus cronie 
+pacman -S --noconfirm acpid ntp dbus cronie wget
 
 systemctl enable acpid
 systemctl enable ntpd
@@ -13,13 +13,17 @@ date
 hwclock -w
 
 # Xorg install
-pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu libinput amd-ucode android-udev pipewire-pulse wireplumber ttf-droid noto-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts alacarte pipewire-jack
+pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu libinput amd-ucode android-udev pipewire-pulse wireplumber ttf-droid noto-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts alacarte pipewire-jack ttf-fira-code
 
 # Gnome install
-pacman -S --noconfirm gnome-shell nautilus gnome-terminal guake gnome-tweak-tool gnome-control-center xdg-user-dirs-gtk gdm gnome-keyring mutter cheese file-roller gedit gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-menus gnome-screenshot gnome-session gnome-settings-daemon gnome-system-monitor gnome-themes-extra
+pacman -S --noconfirm gnome-shell nautilus alacritty guake gnome-tweak-tool gnome-control-center xdg-user-dirs-gtk gdm gnome-keyring mutter cheese file-roller gedit gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-menus gnome-screenshot gnome-session gnome-settings-daemon gnome-system-monitor gnome-themes-extra
 
 # Extra
 pacman -S --noconfirm chromium btop 
+
+# Alacrity setup 
+mkdir -p /home/juansquintero/.config/alacritty
+wget -p /home/juansquintero/.config/alacritty https://raw.githubusercontent.com/juansquintero/arch_scripts/main/alacritty.yml
 
 # yay install 
 pacman -S --needed --noconfirm git base-devel
