2c3cddc5e32c49329b6f2b9602be839337eb5070
Juan Sebastian Quintero <strangelife28@gmail.com>
2022-04-19 11:17:44 -0500

    flatpak and small configs

--- arch-post-install-gnome.sh
+++ arch-post-install-gnome.sh
@@ -16,24 +16,53 @@ hwclock -w
 pacman -S --noconfirm xorg-server xorg-xinit xf86-video-amdgpu libinput amd-ucode android-udev pipewire-pulse wireplumber ttf-droid noto-fonts ttf-jetbrains-mono adobe-source-code-pro-fonts alacarte pipewire-jack ttf-fira-code
 
 # Gnome install
-pacman -S --noconfirm gnome-shell nautilus alacritty guake gnome-tweak-tool gnome-control-center xdg-user-dirs-gtk gdm gnome-keyring mutter cheese file-roller gedit gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-menus gnome-screenshot gnome-session gnome-settings-daemon gnome-system-monitor gnome-themes-extra
+pacman -S --noconfirm gnome-shell nautilus alacritty gnome-tweak-tool gnome-control-center xdg-user-dirs-gtk gdm gnome-keyring mutter cheese file-roller gedit gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-menus gnome-screenshot gnome-session gnome-settings-daemon gnome-system-monitor gnome-themes-extra
 
 # Extra
 pacman -S --noconfirm chromium btop 
 
 # Alacrity setup 
-sudo -u mkdir -p /home/juansquintero/.config/alacritty
-sudo -u juansquintero wget -p /home/juansquintero/.config/alacritty https://raw.githubusercontent.com/juansquintero/arch_scripts/main/alacritty.yml
+mkdir -p /home/juansquintero/.config/alacritty
+sudo -u juansquintero sudo cp /root/alacritty.yml /home/juansquintero/.config/alacritty/
+
+# Ohmyzsh setup base
+sudo -u juansquintero sudo mv /root/.zshrc /home/juansquintero/
 
 # yay install 
 pacman -S --needed --noconfirm git base-devel
 chmod 777 /yay-install.bash
-sudo -u juansquintero /yay-install.bash
+sudo -u juansquintero ./yay-install.bash
 rm -rf /home/juansquintero/yay
+
+# yay package install
 
 sudo -u juansquintero yay -S pfetch-git --noconfirm
 sudo -u juansquintero yay -S nerd-fonts-jetbrains-mono --noconfirm 
+sudo -u juansquintero yay -S touchegg 
 
+# enable touchegg
+
+systemctl enable touchegg.service
+
+# setup touchegg
+
+sudo -u juansquintero $ mkdir -p ~/.config/touchegg && cp -n /root/touchegg.conf ~/.config/touchegg/touchegg.conf
+
+
+
+# flatpak install 
+
+sudo -u juansquintero flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo -u juansquintero flatpak install -y flathub org.gnome.TextEditor
+sudo -u juansquintero flatpak install -y flathub com.spotify.Client
+sudo -u juansquintero flatpak install -y flathub com.discordapp.Discord
+sudo -u juansquintero flatpak install -y flathub com.github.joseexposito.touche
+sudo -u juansquintero flatpak install -y flathub com.mattjakeman.ExtensionManager
+
+
+# Wallpaper
+sudo -u juansquintero mkdir -p /home/juansquintero/Pictures/Wallpaper
+sudo -u juansquintero cp -n /root/wall.png /home/juansquintero/Pictures/Wallpaper
 
 # Enable gnome lockscreen
 systemctl enable gdm 
